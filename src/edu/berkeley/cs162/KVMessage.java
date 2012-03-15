d89d25c6d46e35ab0dfc3d3f12731b7595a5bc15
Prashanth Mohan <prashmohan@gmail.com>
2012-03-15 15:43:12 -0700

    Adding skeleton code

---
+++ src/edu/berkeley/cs162/KVMessage.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ * XML Parsing library for the key-value store
+ * @author Prashanth Mohan (http://www.cs.berkeley.edu/~prmohan)
+ *
+ * Copyright (c) 2011, University of California at Berkeley
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of University of California, Berkeley nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *    
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL PRASHANTH MOHAN BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.berkeley.cs162;
+
+import java.io.FilterInputStream;
+import java.io.InputStream;
+
+
+/**
+ * This is the object that is used to generate messages the XML based messages 
+ * for communication between clients and servers. Data is stored in a 
+ * marshalled String format in this object.
+ */
+public class KVMessage {
+	private String msgType = null;
+	private String key = null;
+	private String value = null;
+
+	public KVMessage(String msgType, String key, String value) {
+		this.msgType = msgType;
+		this.key = key;
+		this.value = value;
+	}
+
+	
+	/* Hack for ensuring XML libraries does not close input stream by default.
+	 * Solution from http://weblogs.java.net/blog/kohsuke/archive/2005/07/socket_xml_pitf.html */
+	private class NoCloseInputStream extends FilterInputStream {
+	    public NoCloseInputStream(InputStream in) {
+	        super(in);
+	    }
+	    
+	    public void close() {} // ignore close
+	}
+	
+	public KVMessage(InputStream input) {
+		// implement me	
+	}
+
+	
+	/**
+	 * Generate the XML representation for this message.
+	 * @return the XML String
+	 */
+	public String toXML() {
+		// implement me
+		return null;
+	}
+}
